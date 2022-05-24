f6e2478d65f3435f7a4abe51092f96bcdf46602f
Bartłomiej Smołucha <bartlomiej@smolucha.pl>
2022-05-24 20:58:05 +0200

    naniesione poprawki czytelnosciowe

--- modul-1/example-1/index.js
+++ modul-1/example-1/index.js
@@ -1,8 +1,12 @@
 const year = 2348;
 
 function checkYear(year) {
-    const leap = new Date(year, 1, 29).getDate() === 29;
-    leap ? console.log(`${year} to rok przestępny!`) : console.log(`${year} to nie jest rok przestepny.`);
+  const leap = new Date(year, 1, 29).getDate() === 29;
+  if (leap) {
+    console.log(`${year} to rok przestępny!`);
+  } else {
+    console.log(`${year} to nie jest rok przestepny.`);
+  }
 }
 
 checkYear(year);
--- modul-1/example-2/index.js
+++ modul-1/example-2/index.js
@@ -1,33 +1,26 @@
-let prompt = require('./node_modules/prompt');
+const prompt = require('prompt');
 
 let schema = {
-    properties: {
-        number: {
-            description: 'Podaj liczbę z zakresu 1-10: '
-        }
-    }
+  properties: {
+    number: {
+      description: 'Podaj liczbę z zakresu 1-10: ',
+    },
+  },
 };
 
 const random = Math.floor(Math.random() * 10) + 1;
 
 function guessNumber() {
+  prompt.start();
 
-    prompt.start();
-
-    prompt.get(schema, function (err, result) {
-        if(parseInt(result.number) !== random) {
-            console.log(`Niestety nie jest to ${result.number}. \nSpróbuj ponownie:`);
-            guessNumber();
-        } else {
-            console.log(`Gratulacje! Moja liczba to ${parseInt(result.number)}!`);
-        }
-    });
-
-  }
+  prompt.get(schema, (_err, result) => {
+    if (parseInt(result.number) !== random) {
+      console.log(`Niestety nie jest to ${result.number}. \nSpróbuj ponownie:`);
+      guessNumber();
+    } else {
+      console.log(`Gratulacje! Moja liczba to ${parseInt(result.number)}!`);
+    }
+  });
+}
 
 guessNumber();
-
-
-
-
-
