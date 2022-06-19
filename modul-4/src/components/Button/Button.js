ce0b5ef64fdf591cafcade1020789278798b900f
Bartłomiej Smołucha <bartlomiej@smolucha.pl>
2022-06-19 15:44:59 +0200

    Zadanie 5 ready

---
+++ modul-4/src/components/Button/Button.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { colors } from "helpers/colors";
+
+const styles = {
+  textAlign: 'center',
+  padding: '.75rem 1rem',
+  width: 'fit-content',
+  lineHeight: '1',
+  backgroundColor: colors.blue,
+  border: `1px solid ${colors.blue}`,
+  color: colors.primary,
+  cursor: 'pointer',
+  transition: 'all .2s ease-in-out'
+}
+
+const Button = React.forwardRef(({ mouseOver, mouseLeave }, ref) => {
+  return (
+    <button type="submit" ref={ ref } onMouseOver={ mouseOver } onMouseLeave={ mouseLeave } style={ styles }>Testowy button</button>
+  )
+});
+
+export default Button;
--- modul-4/src/pages/Main.js
+++ modul-4/src/pages/Main.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+
+import { colors } from "helpers/colors";
 
 import Section from "components/Section";
 import RestaurantForm from "components/RestaurantForm";
 import Validate from "components/Validate";
 import ReactForm from "components/ReactForm";
 import Calculator from "components/Calculator";
+import Button from "components/Button";
 
 const Main = () => {
+  const btnRef = useRef();
+
+  useEffect(() => {
+    btnRef.current.style.background = colors.red;
+    btnRef.current.style.borderColor = colors.red;
+    btnRef.current.style.color = colors.secondary;
+  }, []);
+
+  const mouseHover = () => {
+    btnRef.current.style.background = colors.blue;
+    btnRef.current.style.borderColor = colors.blue;
+    btnRef.current.style.color = colors.primary;
+  }
+
+  const mouseLeave = () => {
+    btnRef.current.style.background = colors.red;
+    btnRef.current.style.borderColor = colors.red;
+    btnRef.current.style.color = colors.secondary;
+  }
   return (
     <>
-      <Section sectionTitle="Rachunek w restauracji">
+      {/* <Section sectionTitle="Rachunek w restauracji">
         <RestaurantForm />
       </Section>
       <Section sectionTitle="Formularz kontaktowy">
@@ -20,6 +42,9 @@ const Main = () => {
       </Section>
       <Section sectionTitle="Domowe finanse">
         <Calculator />
+      </Section> */}
+      <Section sectionTitle="Button - zmiana koloru">
+        <Button ref={ btnRef } mouseOver={ mouseHover } mouseLeave={ mouseLeave } />
       </Section>
     </>
   )
